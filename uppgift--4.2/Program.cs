4cbc348c7c9d566923b4e4669627ecf88cc540b8
Tareq Bilal Al-Asaad <taal04003@utb.vaxjo.se>
2023-10-20 12:48:55 +0200

    Add project files.

---
+++ uppgift--4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int maxNumber = int.MinValue;
+
+        do
+        {
+            Console.Write("Skriv in ett heltal: ");
+            int number;
+
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                if (number > maxNumber)
+                {
+                    maxNumber = number;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ogiltigt heltal. Försök igen.");
+            }
+
+            Console.Write("Vill du skriva in ett till heltal? (j/n): ");
+            string input = Console.ReadLine();
+
+            if (!string.Equals(input, "j", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+        } while (true);
+
+        Console.WriteLine($"Det största talet du skrev in var: {maxNumber}");
+    }
+}
